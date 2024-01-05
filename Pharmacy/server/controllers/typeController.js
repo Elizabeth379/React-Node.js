460ff0ada3851b5f518ce33ed152bb4a1b7f0f4b
Elizabeth <bellator.379@gmail.com>
2024-01-06 00:02:37 +0300

    Type CRUD done

--- Pharmacy/server/controllers/typeController.js
+++ Pharmacy/server/controllers/typeController.js
@@ -2,15 +2,64 @@ const  {Type} = require('../models/models')
 const ApiError = require('../error/ApiError')
 
 class TypeController{
-    async create(req, res){
-        const {name} = req.body
-        const type = await Type.create({name})
-        return res.json(type)
+    async create(req, res, next){
+        try {
+            const { name } = req.body;
+            const type = await Type.create({ name });
+            return res.json(type);
+        } catch (e) {
+            console.error(e);
+            return next(ApiError.internal('Internal Server Error'));
+        }
     }
 
-    async getAll(req, res){
-        const types = await Type.findAll()
-        return res.json(types)
+    async getAll(req, res, next){
+        try {
+            const types = await Type.findAll();
+            return res.json(types);
+        } catch (e) {
+            console.error(e);
+            return next(ApiError.internal('Internal Server Error'));
+        }
+    }
+
+    async update(req, res, next){
+        try {
+            const { id } = req.params;
+            const { name } = req.body;
+
+            const type = await Type.findByPk(id);
+
+            if (!type) {
+                return next(ApiError.badRequest('Такой тип не найден'));
+            }
+
+            await type.update({ name });
+
+            return res.json(type);
+        } catch (e) {
+            console.error(e);
+            return next(ApiError.internal('Internal Server Error'));
+        }
+    }
+
+    async delete(req, res, next){
+        try {
+            const { id } = req.params;
+
+            const type = await Type.findByPk(id);
+
+            if (!type) {
+                return next(ApiError.badRequest('Тип не найден'));
+            }
+
+            await type.destroy();
+
+            return res.json({ message: 'Тип успешно удален' });
+        } catch (e) {
+            console.error(e);
+            return next(ApiError.internal('Internal Server Error'));
+        }
     }
 }
 
--- Pharmacy/server/routes/typeRouter.js
+++ Pharmacy/server/routes/typeRouter.js
@@ -3,7 +3,9 @@ const router = new Router()
 const  typeController = require('../controllers/typeController')
 const checkRole = require('../middleware/checkRoleMiddleware')
 
-router.post('/', checkRole('ADMIN'), typeController.create)
-router.get('/', typeController.getAll)
+router.post('/', checkRole('ADMIN'), typeController.create);
+router.get('/', typeController.getAll);
+router.put('/:id', checkRole('ADMIN'), typeController.update);
+router.delete('/:id', checkRole('ADMIN'), typeController.delete);
 
 module.exports = router
